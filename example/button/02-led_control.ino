066fbeba81d87a05be6954c76908186c9963bd19
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-06-06 13:40:57 +0700

    Control led with button

---
+++ example/button/02-led_control.ino
@@ -0,0 +1,26 @@
+#define LED_PIN 8
+#define BTN_PIN 7
+
+int btn_state;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  btn_state = digitalRead(BTN_PIN);
+  Serial.print("ButtonState:");
+  Serial.println(btn_state);
+
+  if (btn_state == 1){
+    Serial.println("Button Pressed! - LED ON!");
+    digitalWrite(LED_PIN,HIGH);
+  }
+  else {
+    Serial.println("Button Not Pressed! - LED OFF!");
+    digitalWrite(LED_PIN,LOW);
+  }
+  delay(500);
+}
