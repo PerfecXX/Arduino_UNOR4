ace50152601c044cf8c69e34c04cc8c400efa73a
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-06-09 13:16:03 +0700

    Draw a heart with a 2D array

---
+++ example/matrix_led/02-heart_bitmap.ino
@@ -0,0 +1,25 @@
+#define COL_SIZE 12
+#define ROW_SIZE 8
+
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+byte my_frame[ROW_SIZE][COL_SIZE] = {
+  { 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
+  { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void setup() {
+  matrix.begin();
+  matrix.renderBitmap(my_frame, ROW_SIZE, COL_SIZE);
+}
+
+void loop() {
+}
