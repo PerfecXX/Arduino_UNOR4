dab37290731e0504da4d6c718c20ee0afd2c7eca
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-06-09 13:19:22 +0700

    Blink the heart-shaped LED matrix

---
+++ example/matrix_led/03-bitmap_animation.ino
@@ -0,0 +1,39 @@
+#define COL_SIZE 12
+#define ROW_SIZE 8
+
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+byte my_frame[ROW_SIZE][COL_SIZE] = {
+  { 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
+  { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+byte clear_frame[ROW_SIZE][COL_SIZE] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void setup() {
+  matrix.begin();
+}
+
+void loop() {
+  matrix.renderBitmap(my_frame, ROW_SIZE, COL_SIZE);
+  delay(1000);
+  matrix.renderBitmap(clear_frame, ROW_SIZE, COL_SIZE);
+  delay(1000);
+}
