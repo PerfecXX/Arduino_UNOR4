f7a00fa50074506f50d5c42740c9bac3d4fde61b
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-07-12 21:33:53 +0700

    Blink the built-in LED
    Blink the built-in LED

--- example/led/01-builtin_led_blink.ino
+++ example/led/01-builtin_led_blink.ino
@@ -1,3 +1,10 @@
+/*
+Author : 
+Version : 1.0
+Date : 12/07/2024
+Full Schematic : https://www.tinkercad.com/things/jsNyNxq8lcs-arduino-uno-r3-builtin-led?sharecode=0hEjEWMKYy9BL7d-o1PwFixF_IR-AufdBLhPSgFasFI
+*/
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
