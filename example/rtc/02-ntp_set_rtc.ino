229535ea6d48851ee0cf0f009310dd04946dfd92
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-11-05 13:56:30 +0700

    Set date and time with NTP
    Set date and time with NTP

---
+++ example/rtc/02-ntp_set_rtc.ino
@@ -0,0 +1,46 @@
+#include "RTC.h"
+#include <NTPClient.h>
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+
+#define SSID "replace_with_your_ssid"
+#define PASS "replace_with_your_password"
+
+WiFiUDP Udp;
+NTPClient timeClient(Udp);
+RTCTime setting_time;
+RTCTime currentTime;
+
+int timeZoneOffsetHours = 7;  // Bangkok Time
+long unixTime;
+
+void setup() {
+  Serial.begin(9600);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SSID);
+    wifiStatus = WiFi.begin(SSID, PASS);
+    delay(10000);
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  RTC.begin();
+  Serial.println("Starting connection to server...");
+  timeClient.begin();
+  timeClient.update();
+
+  unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
+  Serial.print("Unix time: ");
+  Serial.println(unixTime);
+
+  setting_time = RTCTime(unixTime);
+  RTC.setTime(setting_time);
+
+  RTC.getTime(currentTime);
+  Serial.println("RTC set to " + String(currentTime));
+}
+
+void loop() {}
